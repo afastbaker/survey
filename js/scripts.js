15658fe02399eafecbdb0bd4bc452aafb420150e
Andy Boggs and Thomas Doherty <thomas@>
2018-02-12 20:53:31 -0800

    Style form classes and debug html. In Process. Need to finish page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var name1Input = $("input#name1Input1").val();
+    var name1Input = $("input#name1").val();
     var name2Input = $("input#name2").val();
+    var beverage = $("#beverage").val();
+    console.log(beverage);
+
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    console.log(flavor);
+    //
+    // var dob = $("#born").val();
+    //
+    // var favoriteColor = $("#color").val();
 
 
     $(".name1").text(name1Input);
     $(".name2").text(name2Input);
-
-    var beverage = $("#beverage").val();
+    $(".beverage").text(beverage);
+    $(".flavor").text(flavor);
 
     $("#story").show();
 
